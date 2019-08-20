6554088ad8858a18be7228a12cceeab56f045c87
ryanfelicilda <ryan.g.felicilda@gmil.com>
2019-08-20 10:49:56 -0500

    Added function to roll ALL six dice

--- diceFootball.js
+++ diceFootball.js
@@ -4,40 +4,30 @@
 let coinFlip = Math.floor(Math.random() * 2) == 0 ? 'Heads' : 'Tails';
 console.log(coinFlip);
 
-// Dice roll.
-
+// Single dice roll.
 
 function rollSingleDice(numberOfSides) {
 	let roll = Math.floor(Math.random() * numberOfSides) + 1;
 	return roll;
 }
 
+// Six dice roll.
+
 function rollAllSixDice() {
-	let roll = 
-	// rolls six dice
-	// adds up the rolls
-	// returns the total
+	let rollFour = rollSingleDice(4);
+	let rollSix = rollSingleDice(6);
+	let rollEight = rollSingleDice(8);
+	let rollTen = rollSingleDice(10);
+	let rollTwelve = rollSingleDice(12);
+	let rollTwenty = rollSingleDice(20);
+	let totalRoll = rollFour + rollSix + rollEight + rollTen + rollTwelve + rollTwenty;
+	return totalRoll;
 }
+// Call function.
+let playerOne = rollAllSixDice();
+let playerTwo = rollAllSixDice();
 
-
-
-let playerOneDiceFour = rollSingleDice(4);
-let playerOneDiceSix = rollSingleDice(6);
-let playerOneDiceEight = rollSingleDice(8);
-let playerOneDiceTen = rollSingleDice(10);
-let playerOneDiceTwelve = rollSingleDice(12);
-let playerOneDiceTwenty = rollSingleDice(20);
-
-
-let playerTwoDiceFour = rollSingleDice(4);
-let playerTwoDiceSix = rollSingleDice(6);
-let playerTwoDiceEight = rollSingleDice(8);
-let playerTwoDiceTen = rollSingleDice(10);
-let playerTwoDiceTwelve = rollSingleDice(12);
-let playerTwoDiceTwenty = rollSingleDice(20);
-
-let playerOne = (playerOneDiceFour + playerOneDiceSix + playerOneDiceEight + playerOneDiceTen + playerOneDiceTwelve + playerOneDiceTwenty);
-let playerTwo = (playerTwoDiceFour + playerTwoDiceSix + playerTwoDiceEight + playerTwoDiceTen + playerTwoDiceTwelve + playerTwoDiceTwenty);
+console.log(playerOne,playerTwo)
 
 let continueIteration = true;
 
@@ -48,6 +38,7 @@ while(continueIteration) {
 
 	if(playerOne > playerTwo){
 		console.log("Pass Complete");
+		continueIteration = false;
 	}
 	else if(playerOne < playerTwo){
 		console.log("Pass Incomplete");
